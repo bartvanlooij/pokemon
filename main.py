ba5daeb110b5719e31d3585a2a047e1cceebc97d
bartvanlooij <bartvanlooij@outlook.com>
2022-10-25 17:31:55 +0200

    NU WERKT IE PERFECT

--- main.py
+++ main.py
@@ -9,10 +9,21 @@ import pytesseract
 import math
 from print_pokemon_data import get_pokemon_data
 import pandas as pd
+import numpy as np
+import re
+def check_for_battle(bot : PIL.Image.Image, test_screen : PIL.Image.Image):
+    bot, test_screen = make_images_same_size(test_screen, bot)
+    bot = ImageOps.grayscale(bot)
+    test_screen = ImageOps.grayscale(test_screen)
+    similairity = similarity(bot, test_screen)
+    return similairity
 
 def main():
+    calibrated = False
     print_order = ["Name", "Type 1", "Type 2", "HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed", "Total"]
     df_pokemon = pd.read_csv("pokemon.csv", index_col=0)
+    duplicates = df_pokemon.index.duplicated(keep="first")
+    df_pokemon = df_pokemon[~duplicates]
     df_all_moves = pd.read_csv("df_all_moves.csv", index_col=0)
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
     all_windows = gw.getAllTitles()
@@ -25,20 +36,30 @@ def main():
     bot_now, test_screen = make_images_same_size(battle_format, bot_screen)
     bot_now = ImageOps.grayscale(bot_now)
     test_screen = ImageOps.grayscale(test_screen)
-    similairity = similarity(bot_now,test_screen)
-    if similairity > 0.85:
-        currently_in_battle = True
-    if currently_in_battle:
-        box_coords = find_name_box(top_screen)
-        box_image = top_screen.crop(box_coords)
-        # box_image = Image.open("test_fotos/pokemon_box.png")
-        box_image = ImageOps.grayscale(box_image)
-        pokemon_name = pytesseract.image_to_string(box_image)
-        df_current_pokemon = get_pokemon_data(pokemon_name, df_pokemon, df_all_moves)
-        for element in print_order:
-            print(f"{element}: {df_current_pokemon.loc[element]}")
-    while True:
-        break
+    box_coords = find_name_box(top_screen)
+    calibrated = True
+    pokemon_name = "pickacu"
+    while calibrated:
+        time.sleep(2)
+        screen = screenshot(name_desmume)
+        bot_screen = screen.crop(bot_coords)
+        if (check_for_battle(bot_screen, test_screen) > 0.85):
+            currently_in_battle = True
+        else:
+            currently_in_battle = False
+
+
+        if currently_in_battle:
+
+            top_screen = screen.crop(top_coords)
+            box_image = top_screen.crop(box_coords)
+            box_image = ImageOps.grayscale(box_image)
+            if pytesseract.image_to_string(box_image) != pokemon_name:
+                pokemon_name = pytesseract.image_to_string(box_image)
+                df_current_pokemon = get_pokemon_data(pokemon_name, df_pokemon, df_all_moves)
+                for element in print_order:
+                    print(f"{element}: {df_current_pokemon.loc[element]}")
+
 
 
 
--- print_pokemon_data.py
+++ print_pokemon_data.py
@@ -1,8 +1,11 @@
 
 import pandas as pd
-
+import difflib
 def get_pokemon_data(pokemon : str, df_stats : pd.DataFrame, df_all_moves : pd.DataFrame):
+    print(f"String detected is {pokemon}")
     pokemon = pokemon[0] + pokemon[1:].lower()
     pokemon = pokemon.strip()
-    df_current_pokemon = df_stats[df_stats.columns.difference(["Legendary", "Generation"])].loc[df_stats[df_stats["Name"] == pokemon].index[0]]
+    all_pokemon = df_stats["Name"].tolist()
+    pokemon = difflib.get_close_matches(pokemon, all_pokemon, 1, 0.4)[0]
+    df_current_pokemon = df_stats.loc[df_stats[df_stats["Name"] == pokemon].index[0]]
     return df_current_pokemon
