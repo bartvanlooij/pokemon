2fa35d8ae54c67c4056b5df0ebc01cbfbcd35acb
bartvanlooij <bartvanlooij@outlook.com>
2022-10-27 15:01:33 +0200

    nieuwe dingen

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from compare_images import *
 import pytesseract
 from print_pokemon_data import get_pokemon_data
 import pandas as pd
+import numpy as np
 def check_for_battle(bot : PIL.Image.Image, test_screen : PIL.Image.Image):
     bot, test_screen = make_images_same_size(test_screen, bot)
     bot = ImageOps.grayscale(bot)
@@ -14,12 +15,22 @@ def check_for_battle(bot : PIL.Image.Image, test_screen : PIL.Image.Image):
     similairity = similarity(bot, test_screen)
     return similairity
 
+
+def print_evolution_order(df_current_pokemon):
+    return_string = ""
+    for count in range(8):
+        if type(df_current_pokemon.loc[f"{count}"]) != np.float64:
+            print(type(df_current_pokemon.loc[f"{count}"]))
+            return_string = return_string + df_current_pokemon.loc[f"{count}"]
+        if type(df_current_pokemon.loc[f"{count+1}_{count+2}"]) != np.float64:
+            evolution_req = df_current_pokemon.loc[f'{count + 1}_{count + 2}']
+            return_string = return_string +  f"---{evolution_req}-->"
+
+    return return_string
 def main():
     calibrated = False
     print_order = ["Name", "Type 1", "Type 2", "HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed", "Total"]
     df_pokemon = pd.read_csv("pokemon.csv", index_col=0)
-    duplicates = df_pokemon.index.duplicated(keep="first")
-    df_pokemon = df_pokemon[~duplicates]
     df_all_moves = pd.read_csv("df_all_moves.csv", index_col=0)
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
     all_windows = gw.getAllTitles()
@@ -52,10 +63,13 @@ def main():
             box_image = ImageOps.grayscale(box_image)
             if pytesseract.image_to_string(box_image) != pokemon_name:
                 pokemon_name = pytesseract.image_to_string(box_image)
-                df_current_pokemon = get_pokemon_data(pokemon_name, df_pokemon, df_all_moves)
+                df_current_pokemon = get_pokemon_data(pokemon_name, df_pokemon)
                 print("\n---------------------------------------------\n")
                 for element in print_order:
                     print(f"{element}: {df_current_pokemon.loc[element]}")
+
+                print(print_evolution_order(df_current_pokemon))
+
 
 
 
--- print_pokemon_data.py
+++ print_pokemon_data.py
@@ -1,10 +1,11 @@
-import os
+
 import pandas as pd
 import difflib
-def get_pokemon_data(pokemon : str, df_stats : pd.DataFrame, df_all_moves : pd.DataFrame):
+def get_pokemon_data(pokemon : str, df_stats : pd.DataFrame):
     pokemon = pokemon[0] + pokemon[1:].lower()
     pokemon = pokemon.strip()
-    all_pokemon = df_stats["Name"].tolist()
-    pokemon = difflib.get_close_matches(pokemon, all_pokemon, 1, 0.4)[0]
+    all_pokemon = df_stats["Name"].tolist()[:-1]
+    pokemon = difflib.get_close_matches(pokemon, all_pokemon, 1,0.4)[0]
+    print(f"dichts bij: {pokemon}")
     df_current_pokemon = df_stats.loc[df_stats[df_stats["Name"] == pokemon].index[0]]
     return df_current_pokemon
